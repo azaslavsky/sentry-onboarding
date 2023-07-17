56d3687d5d0ea95796227b2b586c0b2b15e9c746
Alex Zaslavsky <azaslavsky@users.noreply.github.com>
2023-07-17 12:53:28 -0700

    Add transactio instrumentation

--- am_i_prime.js
+++ am_i_prime.js
@@ -13,12 +13,14 @@ function clear() {
 // We purposefully use the brute force method, so that later when we add a transaction big numbers
 // show up as performance regressions.
 function is_it_prime() {
+    const transaction = Sentry.startTransaction({ name: "is_it_prime" });
     const n = document.getElementById("num").value;
     if (n == -1 || n == 0 || n == 1) {
         // return print(n, false);
         throw new Error('Whoops, this is a domain-specific bug!');
     }
 
+    const span = transaction.startChild({ op: "brute_force_portion" });
     let factors = 0;
     for (i = 0; i <= n; i++) {
         if (n % i == 0) {
@@ -26,6 +28,8 @@ function is_it_prime() {
         }
     }
 
+    span.finish();
+    transaction.finish();
     if (factors == 2) {
         return print(n, true);
     }
